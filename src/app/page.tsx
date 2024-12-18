39b31bb719c3f9fc85a6d3ea1d3cf254d00a1480
Ambreen Asif <162163488+Ambreen140187@users.noreply.github.com>
2024-12-18 17:41:38 +0500

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "./components/hero";
+import About from "./components/about";
+import Skill from "./components/skills";
+import Project from "./components/projects";
+import Contact from "./components/contact";
+export default function Home() {
+  return (
+    <div>    <Hero/>
+    <About/>
+    <Skill/>
+    
+    <Project/>
+    <Contact/>
+    
+    </div>
+      );
+}
